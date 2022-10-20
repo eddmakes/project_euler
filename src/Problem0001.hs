8d91987a45dc84ae8360d17e0129cd4d512a4433
eddmakes <edd@schauman-haigh.io>
2022-10-19 18:06:17 -0700

    Complete problem 0001

---
+++ src/Problem0001.hs
@@ -0,0 +1,13 @@
+-- If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
+
+-- Find the sum of all the multiples of 3 or 5 below 1000.
+
+module Problem0001 (answer) where
+
+import Data.List
+
+multiplesOf :: Integer -> [Integer]
+multiplesOf n = [i | i <- [1..999], i `mod` n == 0]
+
+answer :: IO ()
+answer = print $ sum . Data.List.nub $ multiplesOf 3 ++ multiplesOf 5
